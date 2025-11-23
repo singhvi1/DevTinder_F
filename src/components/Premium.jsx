40f5691ea15f3f44e0b59fc7be968be3f8e87790
singhvi1 <singvi182003@gmail.com>
2025-11-23 19:58:25 +0530

    feat: Add premium user verification and update UI in Premium component

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,7 +1,19 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constant";
+import { useEffect, useState } from "react";
 
 const Premium = () => {
+  const [isUserPremium, setIsUserPremium] = useState(false);
+  useEffect(() => {
+    verifyPremiumUser()
+  }, [])
+
+  const verifyPremiumUser = async () => {
+    const res = await axios(BASE_URL + "/premium/verify", { withCredentials: true })
+    if (res.data.isPremium) {
+      setIsUserPremium(true);
+    }
+  }
   const handleBuyClick = async (type) => {
     const order = await axios.post(
       BASE_URL + "/payment/create",
@@ -24,48 +36,54 @@ const Premium = () => {
       theme: {
         color: "#F37254",
       },
+      handler: verifyPremiumUser,
     };
     const rzp = new window.Razorpay(options);
     rzp.open();
     console.log(order);
   };
-  return (
-    <div className="flex justify-center  m-10 py-20">
-      <div className="flex w-full flex-col lg:flex-row">
-        <div className="card bg-base-300 rounded-box grid h-80  grow place-items-center">
-          <h1 className="font-bold text-3xl ">Silver MemberShip</h1>
-          <ul>
-            <li># Chat with other people</li>
-            <li># 100 connection Requests per day</li>
-            <li># Blue Tick</li>
-            <li># 3 Months</li>
-          </ul>
-          <button
-            className="btn btn-primary"
-            onClick={() => handleBuyClick("silver")}
-          >
-            Buy Silver
-          </button>
-        </div>
-        <div className="divider lg:divider-horizontal">OR</div>
-        <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
-          <h2 className="text-3xl font-bold">Gold MemeberShip</h2>
-          <ul>
-            <li># Chat with other people</li>
-            <li># Unlimited connection Requests per day</li>
-            <li># Blue Tick</li>
-            <li># 6 Months</li>
-          </ul>
-          <button
-            className="btn btn-secondary"
-            onClick={() => handleBuyClick("gold")}
-          >
-            Buy Gold
-          </button>
+
+  return isUserPremium ?
+    <div className="flex h-screen justify-center items-center">
+      <h1 className="font-bold text-3xl">You are already a premium Memeber</h1>
+    </div> :
+    (
+      <div className="flex justify-center  m-10 py-20">
+        <div className="flex w-full flex-col lg:flex-row">
+          <div className="card bg-base-300 rounded-box grid h-80  grow place-items-center">
+            <h1 className="font-bold text-3xl ">Silver MemberShip</h1>
+            <ul>
+              <li># Chat with other people</li>
+              <li># 100 connection Requests per day</li>
+              <li># Blue Tick</li>
+              <li># 3 Months</li>
+            </ul>
+            <button
+              className="btn btn-primary"
+              onClick={() => handleBuyClick("silver")}
+            >
+              Buy Silver
+            </button>
+          </div>
+          <div className="divider lg:divider-horizontal">OR</div>
+          <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
+            <h2 className="text-3xl font-bold">Gold MemeberShip</h2>
+            <ul>
+              <li># Chat with other people</li>
+              <li># Unlimited connection Requests per day</li>
+              <li># Blue Tick</li>
+              <li># 6 Months</li>
+            </ul>
+            <button
+              className="btn btn-secondary"
+              onClick={() => handleBuyClick("gold")}
+            >
+              Buy Gold
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default Premium;
